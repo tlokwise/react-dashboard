34a3370dfde59dcfb1623324546f55865fe8a2f0
tlokwise <tso.mtlokoa@outlook.com>
2023-12-08 15:20:51 +0200

    add delete side bin

--- src/components/SideBin.tsx
+++ src/components/SideBin.tsx
@@ -1,8 +1,16 @@
+import React from "react";
 import { Trash } from "react-bootstrap-icons";
-export default function SideBin() {
+
+interface SideBinProps {
+  onRowDelete : (event: React.MouseEvent<HTMLElement, MouseEvent>) => void
+}
+
+export default function SideBin({onRowDelete} : SideBinProps){
+
   return (
-    <td className="delete-row-side-bin">
+    <td className="delete-row-side-bin" onClick={onRowDelete}>
       <Trash className="side-bin" />
     </td>
   );
-}
+} 
+
